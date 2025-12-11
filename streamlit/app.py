22e68b04d76a0e35e0260a26c746f895a3f89eff
Артур Камалов <artkmlv@yandex.ru>
2025-12-11 14:13:06 +0300

    Change streamlit/app.py: add title and description

--- streamlit/app.py
+++ streamlit/app.py
@@ -10,10 +10,17 @@ load_dotenv()
 API_URL = 'https://api.artkmlv.ru/'
 
 
-st.title("Predict income")
-st.write("Введите данные человека:")
-
 categories = requests.get(API_URL + "/person_categories").json()
+
+st.title("🔮 Прогноз уровня дохода")
+
+st.markdown("""
+Это приложение использует модель машинного обучения, чтобы предсказать, превышает ли годовой доход человека **50,000$**.
+
+Чтобы получить предсказание, заполните данные ниже.  
+Модель обучена на датасете [Adult Census Income](https://www.kaggle.com/datasets/uciml/adult-census-income/data) и учитывает социально-демографические характеристики.
+""")
+
 
 with st.container():
 
@@ -22,16 +29,14 @@ with st.container():
     with col1:
         workclass = st.selectbox("Workclass", categories["workclass"])
         education = st.selectbox("Education", categories['education'])
-        marital_status = st.selectbox(
-            "Marital Status", categories['marital_status'])
+        marital_status = st.selectbox("Marital Status", categories['marital_status'])
         occupation = st.selectbox("Occupation", categories['occupation'])
         relationship = st.selectbox("Relationship", categories['relationship'])
         race = st.selectbox("Race", categories['race'])
-        sex = st.selectbox("Sex", categories['sex'])
 
     with col2:
+        sex = st.selectbox("Sex", categories['sex'])
         age = st.number_input("Age", 18, 100)
-        fnlwgt = st.number_input("FNLWGT", 0)
         education_num = st.number_input("Education-num", 1, 16)
         capital_gain = st.number_input("Capital gain", 0)
         capital_loss = st.number_input("Capital loss", 0)
@@ -40,7 +45,7 @@ with st.container():
 if st.button("Сделать предсказание"):
     data = {
         "age": age,
-        "fnlwgt": fnlwgt,
+        "fnlwgt": 100000,
         "education_num": education_num,
         "capital_gain": capital_gain,
         "capital_loss": capital_loss,
